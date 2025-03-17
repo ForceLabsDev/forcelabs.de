32d330383500de2e42db6f0a7e50f58327ee3f51
Lennart Franke <10079938+lennartfranke@users.noreply.github.com>
2025-03-17 21:23:39 +0100

    change to app router

---
+++ app/layout.tsx
@@ -0,0 +1,62 @@
+import Link from "next/link";
+import React from "react";
+import {Metadata, Viewport} from "next";
+
+import "../styles/style.css";
+
+interface Props {
+    children?: React.ReactNode;
+}
+
+export const metadata: Metadata = {
+    title: 'ForceLabs » Wartungsarbeiten «',
+    description: 'ForceLabs - grants you the force',
+}
+
+export const viewport: Viewport = {width: "device-width", initialScale: 1.0}
+
+export default function Layout({children}: Props) {
+    return (
+        <html>
+        <body>
+        <div id="main">
+            <header className="header-container">
+                <div className="header">
+                    <div className="header-item header-item__logo">
+                        <div className="logo">
+                            <Link href="/">
+                                ForceLabs
+                            </Link>
+                        </div>
+                        <div className="sublogo">
+                            <Link href="/">
+                                grants you the force.
+                            </Link>
+                        </div>
+                    </div>
+
+                    <div className="header-item header-pagetitle">
+                        &raquo; Wartungsarbeiten &laquo;
+                    </div>
+                </div>
+            </header>
+
+            <div className="main-container">{children}</div>
+
+            <footer className="footer-container">
+                <div>
+                    &copy; 2009 - {new Date().getFullYear()}{" "}
+                    <Link href="/">
+                        ForceLabs
+                    </Link>{" "}
+                    |{" "}
+                    <Link href="/impressum">
+                        Impressum
+                    </Link>
+                </div>
+            </footer>
+        </div>
+        </body>
+        </html>
+    );
+};
---
+++ app/page.tsx
@@ -0,0 +1,29 @@
+export default function Page() {
+    return (
+        <>
+            <div className="info-item">
+                <div className="info-item__inner">
+                    <strong>Achtung:</strong> Wir machen neues Zeug!
+                </div>
+            </div>
+            <div className="content-item full-item">
+                <div className="content-item__inner">
+                    <h2>Weitere Informationen</h2>
+                    <ul>
+                        <li>
+                            Besuche uns auf GitHub:{" "}
+                            <a href="https://github.com/ForceLabsDev">
+                                github.com/ForceLabsDev
+                            </a>
+                        </li>
+                        <li>
+                            Das &raquo;ForceCMS&laquo; erhält momentan keinen Support mehr und
+                            kann nicht mehr heruntergeladen werden.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    );
+};
+
